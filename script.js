455e1718823c70d132089e332f8e96cb08639021
Юрий <iorgos.grek@gmail.com>
2019-04-12 16:07:09 +0300

    Это наш первый коммит

---
+++ script.js
@@ -0,0 +1,32 @@
+let money = +prompt("Ваш бюджет на месяц?", " "),
+    time = prompt("Введите дату в формате ГГГГ-ММ-ДД", "2019-04-11");
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", " "),
+        b = prompt("Во сколько обойдется?", " ");
+    if ( (typeof(a)) === "string" && (typeof(a)) != null && (typeof(b)) != null
+        && a != "" && b != "" && a.length < 50 ) {
+            console.log("done");
+            appData.expenses.a = b;
+    }     
+}
+appData.moneyPerDay = appData.budget / 30;
+alert("Ваш ежедневный бюджет составляет: " + appData.moneyPerDay + " рублей");
+if (appData.moneyPerDay < 100) {
+    console.log("Миимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay <= 2000) {
+    console.log("Средний уровень достатка"); 
+} else if(appData.moneyPerDay < 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Произошла ошибка");
+}
